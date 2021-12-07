22e5c017b7b031498ba681b2a08c43399fd4c88a
babhinav <babhinav@intraedge.com>
2021-12-07 19:08:59 +0530

    Sqr: test - commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express")
+
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Smart QR' });
-});
+router.get("/", (req, res, next) => {
+ 
+     res.json({ title: "Smart QR" })
+})
 
-module.exports = router;
+module.exports = router
